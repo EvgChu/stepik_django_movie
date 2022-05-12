4820ba7b91e0604b1053372be276fb142be6978b
Evg Chu <chuevgenii@gmail.com>
2022-05-12 23:21:44 +0300

    studied the admin panel

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Movie
+
+
+@admin.register(Movie) # Can use decorator
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', "rating_name"]
+    list_editable = ['rating', 'year' ] # don't use name 
+    ordering = ['-rating'] # for sorting
+    list_per_page = 30 # pagination
+
+    @admin.display(ordering='rating', description="Status")
+    def rating_name(self, movie:Movie):
+        if movie.rating > 90:
+            return "Great"
+        elif movie.rating > 80:
+            return "Nice"
+        elif movie.rating > 60:
+            return "Good"
+        else:
+            return "Bad"
+
 
 # Register your models here.
+# admin.site.register(Movie,MovieAdmin )
