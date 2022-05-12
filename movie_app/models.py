4dedc5d8a462309ac81cd6909abb1e949376641f
EvgChu <chuevgenii@gmail.com>
2022-05-12 11:12:38 +0300

    did lessons 4-11

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -12,3 +12,6 @@ class Movie(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name} {self.rating}"
+
+    def get_url(self):
+        return  reverse('movie-detail', args=[self.id])
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 # Create your views here.
+
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    return render(request,"all_movies.html",context={"movies":movies})
+
+
+def show_one_movie(request, id_movie):
+    movie = get_object_or_404(Movie,id=id_movie)
+    context = {
+        "movie": movie
+        }
+    return render(request,"one_movie.html",context=context)
