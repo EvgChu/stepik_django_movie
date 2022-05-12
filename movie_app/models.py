88d9ba58accfcb1fd49b387ded6ba46cdf9130cc
EvgChu <chuevgenii@gmail.com>
2022-05-12 09:38:33 +0300

    Added simple model

--- movie_app/models.py
+++ movie_app/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class Movie(models.Movie):
+class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=100000)
+
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.rating}"
