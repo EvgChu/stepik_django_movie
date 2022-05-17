1b484df069f1a40e2ec9124f0eb32a5fb40cadcf
EvgChu <chuevgenii@gmail.com>
2022-05-17 13:38:36 +0300

    fixed static file

--- feedback/forms.py
+++ feedback/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class FeedbackForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(label="Name",max_length=7,min_length=2)
     surname = forms.CharField()
     feedback = forms.CharField(widget=forms.Textarea(attrs={'rows':3,'cols':20}))
+    rating = forms.IntegerField(max_value=5,min_value=1)
